9414457f96284d8fac299c428c2d259c603f96bf
Andy Guinder <andreabelen.guinder@gmail.com>
2026-07-10 18:32:06 -0300

    mejoras luego de correcciond e tarea, incorporo prop alt para las fotos, elimino info

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,31 @@ import fotoLuna from "./assets/luna.jpg";
 import fotoNosotros from "./assets/nosotros.jpg";
 import fotoAlma from "./assets/alma.jpg";
 
+const altLuna = "Imagen de perra color negro, herencia de pitbull, orejona, le falta un ojito, con un juguete en la boca.";
+const altNosotros = "Imagen de una pareja abrazados, ella rubia de ojos claros, él castaño con barba y rulos.";
+const altAlma = "Imagen de perra cachorra, color negro, con las patitas blancas de adelante como botitas, acostada.";
+
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>Mi Familia</h1>
+      <h1>Mi Familia</h1>
       <section className="contenedor-tarjetas">
         <Tarjeta
           nombre={"Luna"}
           profesion={"Perrhija: Primera bebé"}
           imagen={fotoLuna}
+          alt={altLuna}
           descripcion={
             "Bebé perruna que integra la familia desde el 05/03/2023"
+
           }
         />
         <Tarjeta
           nombre={"Seba y Andy"}
           profesion={"Padres"}
           imagen={fotoNosotros}
+          alt={altNosotros}
           descripcion={
             "Pareja formal, a punto de contraer matrimonio el 03/07/26"
           }
@@ -33,6 +39,7 @@ function App() {
           nombre={"Alma"}
           profesion={"Perrhija: Segunda bebé"}
           imagen={fotoAlma}
+          alt={altAlma}
           descripcion={
             "Bebé perruna que integra la familia desde el 26/05/2026"
           }
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,14 +1,14 @@
 
 import React from "react";
 
-function Tarjeta( {nombre, profesion, imagen, descripcion} ) {
+function Tarjeta( {nombre, profesion, imagen, descripcion, alt} ) {
 
 
   return (
 
       <div className='tarjeta'>
         <h2>{nombre}</h2>
-        <img src={imagen} alt={nombre} />
+        <img src={imagen} alt={alt} />
         <h3>{profesion}</h3>
         <p>{descripcion}</p>
       </div>
